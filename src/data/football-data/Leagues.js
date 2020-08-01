41e95bfdd311f10e65941e797dc1c2bd74f5c712
Adriano Flach <adriano.araujo@philips.com>
2020-08-01 15:00:04 -0300

    added leagues

--- src/data/football-data/Leagues.js
+++ src/data/football-data/Leagues.js
@@ -1,5 +1,27 @@
 import apis from '../Apis';
 
-export default [
-    {key: 'bl1920', value: 'Bundesliga (2019-20)', api: apis.footballData, clubsUrl: 'competitions/2002/standings?season=2019&standingType=TOTAL', matchesUrl: 'competitions/2002/matches?season=2019'}
-]
+const leagues = [
+    {code: 'ger', name: 'Bundesliga (GER - 2019-20)', id: 2002},
+    {code: 'braa', name: 'Série A (BRA - 2019-20)', id: 2013},
+    {code: 'eng', name: 'Premier League (ENG - 2019-20)', id: 2021},
+    {code: 'fra', name: 'Ligue 1 (FRA - 2019-20)', id: 2015},
+    {code: 'ita', name: 'Serie A (ITA - 2019-20)', id: 2019},
+    {code: 'por', name: 'Primeira Liga (POR - 2019-20)', id: 2017},
+    {code: 'esp', name: 'Primera Division (ESP - 2019-20)', id: 2014}
+];
+
+let exportableData = [];
+
+for (const league of leagues) {
+    exportableData.push({
+        key: league.code,
+        value: league.name,
+        api: apis.footballData,
+        clubsUrl: `competitions/${league.id}/standings?season=2019&standingType=TOTAL`,
+        matchesUrl: `competitions/${league.id}/matches?season=2019`
+    });
+}
+
+console.log(exportableData);
+
+export default exportableData;

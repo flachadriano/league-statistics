e98033fe4858a4491f7fe5151813f20e18673684
Adriano Flach <adriano.araujo@philips.com>
2020-08-02 20:29:05 -0300

    statistics last matches

--- src/components/club/LastMatches.vue
+++ src/components/club/LastMatches.vue
@@ -1,34 +1,49 @@
 <template>    
     <div class="card m-1">
         <h3>Last 6 matches</h3>
-        <div>
+        <div class="mb-2">
             <template v-for="match in (matches||[])">
-                <span :key="match.date" v-if="match.draw"> D</span>
-                <span :key="match.date" v-else-if="match.win"> W</span>
-                <span :key="match.date" v-else> L</span>
+                <span :key="match.date" class="bg-secondary m-1 pr-2 pl-2 pt-1 pb-1 rounded" v-if="match.draw">D</span>
+                <span :key="match.date" class="bg-success m-1 pr-2 pl-2 pt-1 pb-1 rounded" v-else-if="match.win">W</span>
+                <span :key="match.date" class="bg-danger m-1 pr-2 pl-2 pt-1 pb-1 rounded" v-else>L</span>
             </template>
         </div>
-        <table>
-            <thead>
+        <table class="mb-1">
+            <thead class="border-bottom">
                 <td></td>
-                <td>Scored</td>
-                <td>Against</td>
+                <td>Scored goals</td>
+                <td>Goals against</td>
             </thead>
             <tbody>
                 <tr>
-                    <td>First half</td>
+                    <td>Matches in first half</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + (m.teamScoreFirstHalf > 0 ? 1 : 0), 0) }}</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + (m.teamAgainstFirstHalf > 0 ? 1 : 0), 0) }}</td>
                 </tr>
+                <tr class="border-bottom">
+                    <td>Max in first half</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamScoreFirstHalf)) }}</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamAgainstFirstHalf)) }}</td>
+                </tr>
                 <tr>
-                    <td>Second half</td>
+                    <td>Matches in second half</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + ((m.teamScored - m.teamScoreFirstHalf) > 0 ? 1 : 0), 0) }}</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + ((m.teamAgainst - m.teamAgainstFirstHalf) > 0 ? 1 : 0), 0) }}</td>
                 </tr>
+                <tr class="border-bottom">
+                    <td>Max in second half</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamScored - m.teamScoreFirstHalf)) }}</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamAgainst - m.teamAgainstFirstHalf)) }}</td>
+                </tr>
                 <tr>
-                    <td>In the match</td>
+                    <td>Matches</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + (m.teamScored > 0 ? 1 : 0), 0) }}</td>
                     <td>{{ (matches||[]).reduce((acc, m) => acc + (m.teamAgainst > 0 ? 1 : 0), 0) }}</td>
+                </tr>
+                <tr>
+                    <td>Max in a match</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamScored)) }}</td>
+                    <td>{{ Math.max(...(matches||[]).map(m => m.teamAgainst)) }}</td>
                 </tr>
             </tbody>
         </table>
@@ -36,11 +51,11 @@
             <tbody>
                 <tr v-for="match in (matches||[])" :key="match.date">
                     <td>{{ match.date }}</td>
-                    <td :class="home(match)"> {{ match.team1 }}</td>
+                    <td> {{ match.team1 }}</td>
                     <td> {{ match.score1 }}</td>
                     <td> X </td>
                     <td> {{ match.score2 }}</td>
-                    <td :class="visitor(match)"> {{ match.team2 }}</td>
+                    <td> {{ match.team2 }}</td>
                 </tr>
             </tbody>
         </table>
@@ -50,18 +65,6 @@
 <script>
 export default {
     props: ['club'],
-    methods: {
-        home(match) {
-            if (match.home && !match.draw) {
-                return match.win ? 'bg-primary' : 'bg-secondary';
-            }
-        },
-        visitor(match) {
-            if (!match.home && !match.draw) {
-                return match.win ? 'bg-primary' : 'bg-secondary';
-            }
-        },
-    },
     asyncComputed: {
         async matches() {
             return await this.club.lastMatches(6);

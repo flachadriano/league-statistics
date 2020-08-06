f6432818b76ae3e79f441ce491fc760e229aaca8
Adriano Flach <adriano.araujo@philips.com>
2020-08-05 23:19:15 -0300

    api and league with store

--- src/components/Leagues.vue
+++ src/components/Leagues.vue
@@ -1,49 +1,42 @@
 <template>
     <div class="d-flex justify-content-center m-2">
         <div class="d-flex">
-            <select class="custom-select" v-model="api">
+            <select class="custom-select" :value="api" @input="changeApi">
                 <option v-for="item in apis" :key="item" :value="item">{{ item }}</option>
             </select>
-            <select class="custom-select" v-model="selected">
-                <option selected value="0">Select the league</option>
-                <option v-for="league in leagues" :key="league.key" :value="league.key">{{ league.value }}</option>
+            <select class="custom-select" :value="league" @input="changeLeague">
+                <option value="0">Select the league</option>
+                <option v-for="item in leagues" :key="item.key" :value="item.key">{{ item.value }}</option>
             </select>
         </div>
     </div>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
-import apis from '../data/Apis'
-import footballdbLeagues from '../data/footballdb/Leagues'
-import footballDataLeagues from '../data/football-data/Leagues'
+import { mapGetters, mapMutations } from 'vuex'
 import { loadLeagueResources } from '../data/League'
 
 export default {
-    data() {
-        return {
-            selected: 0,
-            leagues: [...footballdbLeagues],
-        }
-    },
     computed: {
         ...mapGetters([
             'apis',
-            'api'
+            'api',
+            'leagues',
+            'league',
+        ])
+    },
+    methods: {
+        ...mapMutations([
+            'changeApi',
+            'changeLeague',
         ])
     },
     watch: {
-        api(value) {
-            if (value == apis.footballdb) {
-                this.leagues = footballdbLeagues;
-            } else {
-                this.leagues = footballDataLeagues;
-            }
-            this.selected = 0;
-            this.$emit('api', value);
+        api(api) {
+            this.$emit('api', api);
         },
-        selected(value) {
-            const league = this.leagues.find(l => l.key == value);
+        league(value) {
+            const league = this.$store.state.leagues.find(l => l.key == value);
             this.$emit('league', loadLeagueResources(this.api, league));
         }
     }
--- src/store.js
+++ src/store.js
@@ -1,22 +1,40 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
 import apis from './data/Apis'
+import footballdbLeagues from './data/footballdb/Leagues'
+import footballDataLeagues from './data/football-data/Leagues'
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
     state: {
         apis,
-        api: 'footballdb'
+        api: apis.footballdb,
+        leagues: footballdbLeagues,
+        league: 0,
     },
     getters: {
         apis: state => state.apis,
-        api: state => state.api
+        api: state => state.api,
+        leagues: state => state.leagues,
+        league: state => state.league,
     },
     mutations: {
-        changeApi: (state, api) => {
-            state.api = api
+        changeApi: (state, apiEl) => {
+            const api = apiEl.target.value;
+            state.api = api;
+
+            if (api == apis.footballdb) {
+                state.leagues = footballdbLeagues;
+            } else {
+                state.leagues = footballDataLeagues;
+            }
+
             state.league = 0;
-        }
+        },
+        changeLeague: (state, leagueEl) => {
+            const league = leagueEl.target.value;
+            state.league = league;
+        },
     }
 });

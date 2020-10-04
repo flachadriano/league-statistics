b3826ccd2ddf86d55cb1f9abc5ad5bf9aa3370f8
Adriano Flach <adriano.araujo@philips.com>
2020-10-04 07:15:31 -0300

    updating football-data to 2020

--- src/data/football-data/Leagues.js
+++ src/data/football-data/Leagues.js
@@ -1,11 +1,11 @@
 const leagues = [
-    {code: 'ger', name: 'Bundesliga (GER - 2019-20)', id: 2002},
-    {code: 'bra', name: 'Série A (BRA - 2020)', id: 2013, year: 2020},
-    {code: 'eng', name: 'Premier League (ENG - 2019-20)', id: 2021},
-    {code: 'fra', name: 'Ligue 1 (FRA - 2020-21)', id: 2015, year: 2020},
-    {code: 'ita', name: 'Serie A (ITA - 2019-20)', id: 2019},
-    {code: 'por', name: 'Primeira Liga (POR - 2019-20)', id: 2017},
-    {code: 'esp', name: 'Primera Division (ESP - 2019-20)', id: 2014}
+    {code: 'ger', name: 'Bundesliga (GER - 2020-21)', id: 2002},
+    {code: 'bra', name: 'Série A (BRA - 2020-21)', id: 2013},
+    {code: 'eng', name: 'Premier League (ENG - 2020-21)', id: 2021},
+    {code: 'fra', name: 'Ligue 1 (FRA - 2020-21)', id: 2015},
+    {code: 'ita', name: 'Serie A (ITA - 2020-21)', id: 2019},
+    {code: 'por', name: 'Primeira Liga (POR - 2020-21)', id: 2017},
+    {code: 'esp', name: 'Primera Division (ESP - 2020-21)', id: 2014}
 ];
 
 let exportableData = [];
@@ -14,8 +14,8 @@ for (const league of leagues) {
     exportableData.push({
         key: league.code,
         value: league.name,
-        clubsUrl: `competitions/${league.id}/standings?season=${league.year||2019}&standingType=TOTAL`,
-        matchesUrl: `competitions/${league.id}/matches?season=${league.year||2019}`
+        clubsUrl: `competitions/${league.id}/standings?season=${2020}&standingType=TOTAL`,
+        matchesUrl: `competitions/${league.id}/matches?season=${2020}`
     });
 }
 
